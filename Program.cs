5845430c676869fd168e6452fcf251bbaa884e53
KAIQUE DOS SANTOS SOUSA <kaique.ssousa3@senacsp.edu.br>
2025-11-06 16:46:23 -0300

    maratona exercicio 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+1. Saudação com Nome
+Objetivo: Declarar variáveis, usar concatenação e interpolação.
+Descrição: Crie um script que declare uma variável para o nome de 
+uma pessoa e exiba uma saudação personalizada, por exemplo: "Olá, 
+João! Bem-vindo ao nosso site!".
+Dica: Utilize tanto a concatenação (.) quanto a interpolação dentro de 
+uma string com aspas duplas.
+*/
+
+string nome = "João";
+
+Console.WriteLine($"Olá, {nome}! Bem - vindo ao nosso site!");
